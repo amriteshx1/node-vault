affcfefb7cd5a14fa20825132dc7e4a26cd14fa2
amriteshx1 <amriteshx18@gmail.com>
2025-04-20 15:12:37 +0530

    Updated login to use email instead of username for authentication

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -3,30 +3,33 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require('bcryptjs');
 
-
 function initialize(passport) {
     passport.use(
-      new LocalStrategy(
-        async (username, password, done) => {
-          const user = await prisma.user.findUnique({ where: { username } });
-          if (!user) return done(null, false, { message: "No user found" });
-  
-          const match = await bcrypt.compare(password, user.password);
-          if (!match) return done(null, false, { message: "Incorrect password" });
-  
-          return done(null, user);
-        }
-     )
-  );
+        new LocalStrategy(
+            {
+                usernameField: 'email', 
+                passwordField: 'password',
+            },
+            async (email, password, done) => {
+                const user = await prisma.user.findUnique({ where: { email } });
+                if (!user) return done(null, false, { message: "No user found with that email" });
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) return done(null, false, { message: "Incorrect password" });
 
-  passport.deserializeUser(async (id, done) => {
-    const user = await prisma.user.findUnique({ where : { id }});
-    done(null, user);
-  });
+                return done(null, user);
+            }
+        )
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        const user = await prisma.user.findUnique({ where: { id } });
+        done(null, user);
+    });
 }
 
 module.exports = initialize;
