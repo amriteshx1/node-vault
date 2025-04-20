5ecf411fc87c95b0cbaf36cdbd7c4be2a25c2d90
amriteshx1 <amriteshx18@gmail.com>
2025-04-20 11:59:16 +0530

    Add Passport.js authentication config with LocalStrategy and session serialization

---
+++ config/passportConfig.js
@@ -0,0 +1,32 @@
+const LocalStrategy = require('passport-local').Strategy;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcryptjs');
+
+
+function initialize(passport) {
+    passport.use(
+      new LocalStrategy(
+        async (username, password, done) => {
+          const user = await prisma.user.findUnique({ where: { username } });
+          if (!user) return done(null, false, { message: "No user found" });
+  
+          const match = await bcrypt.compare(password, user.password);
+          if (!match) return done(null, false, { message: "Incorrect password" });
+  
+          return done(null, user);
+        }
+     )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    const user = await prisma.user.findUnique({ where : { id }});
+    done(null, user);
+  });
+}
+
+module.exports = initialize;
